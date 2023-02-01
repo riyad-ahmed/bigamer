65f064be0c87aa0fc058d4bcb7d07113cba7f106
riyadahmed2809@gmail.com <92542706+riyad-ahmed@users.noreply.github.com>
2023-02-01 14:38:25 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 5,
+  spaceBetween: 30,
+  loop: true,
+  centeredSlides: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
