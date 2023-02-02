00dbdab510857a76093b0fdfc073c849e3d1ac6a
riyadahmed2809@gmail.com <92542706+riyad-ahmed@users.noreply.github.com>
2023-02-03 03:22:05 +0600

    blog add

---
+++ blog.js
@@ -0,0 +1,15 @@
+var swiper = new Swiper(".mySwiper", {
+  cssMode: true,
+  loop: true,
+  
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  mousewheel: true,
+  keyboard: true,
+});
